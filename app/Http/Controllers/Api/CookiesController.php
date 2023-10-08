bfea75e38e329fb0ae67bd73d4a672676f0c1f6f
MoAlshaka <mohamedalshaka330@gmail>
2023-10-08 09:37:55 -0700

    finshed cookies database & controller

--- app/Http/Controllers/Api/CookiesController.php
+++ app/Http/Controllers/Api/CookiesController.php
@@ -5,15 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Models\Cookie;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Support\Facades\Validator;
 
 
 class CookiesController extends Controller
 {
     public function get_cookies(){
-        $cookies = Cookie::pluck('value', 'id');
-        return $cookies;
+        $cookies = Cookie::all();
+        $formattedCookies = [];
+
+        foreach ($cookies as $cookie) {
+            $formattedCookie = [
+                'collectionId' => 'qbbfpzddtydrp5p', // You can set this value as needed
+                'collectionName' => 'cookies', // You can set this value as needed
+                'created' => $cookie->created_at->toIso8601String(),
+                'id' => $cookie->id,
+                'name' => $cookie->name,
+                'updated' => $cookie->updated_at->toIso8601String(),
+                'value' => json_decode($cookie->value, true), // Assuming value is stored as JSON
+            ];
+
+            $formattedCookies[] = $formattedCookie;
+        }
+
+        return response()->json(['items' => $formattedCookies]);
     }
+
+
 
     public function store(Request $request){
         $valdiator=Validator::make($request->all(),[
